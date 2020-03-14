18a99bb987f335c124903bd93b3351e4b2121ecd
Margui <margui.berjon@gmail.com>
2020-03-14 21:34:45 +0100

    Add 2.11 6

---
+++ 2-11-Peticiones-al-servidor-2/Ejercicio6/main.js
@@ -0,0 +1,20 @@
+let form = document.querySelector('.js-form');
+let paragraph = document.querySelector('.js-data');
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+function writeName(ev) {
+  const name = inputs[0].value;
+  const surname = inputs[1].value;
+  paragraph.innerHTML = name + ' ' + surname;
+  localStorage.setItem('inputs', JSON.stringify({ name: name, surname: surname }));
+}
+
+const fieldsLoaded = JSON.parse(localStorage.getItem('inputs'));
+
+paragraph.innerHTML = fieldsLoaded.name + ' ' + fieldsLoaded.surname;
+
+inputs[0].value = fieldsLoaded.name;
+inputs[1].value = fieldsLoaded.surname;
+
+form.addEventListener('keyup', writeName);
