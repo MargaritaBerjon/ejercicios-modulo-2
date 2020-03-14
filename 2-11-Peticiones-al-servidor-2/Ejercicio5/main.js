c92c637a4fe9adaa98b2dafad3b79c13dbda6308
Margui <margui.berjon@gmail.com>
2020-03-14 19:48:31 +0100

    2-11 .5 last point

--- 2-11-Peticiones-al-servidor-2/Ejercicio5/main.js
+++ 2-11-Peticiones-al-servidor-2/Ejercicio5/main.js
@@ -2,13 +2,15 @@ let theme = document.querySelector('.js-themechoose');
 let body = document.querySelector('body');
 
 function themeChoose(ev) {
-  if (ev.target.value === 'light-text') {
-    body.classList.add('light-text');
-    body.classList.remove('dark-text');
+  if (ev.target.value === 'light') {
+    body.classList.add('light');
+    body.classList.remove('dark');
   } else {
-    body.classList.add('dark-text');
-    body.classList.remove('light-text');
+    body.classList.add('dark');
+    body.classList.remove('light');
   }
+  localStorage.setItem('theme', ev.target.value);
 }
 
 theme.addEventListener('change', themeChoose);
+body.classList.add(localStorage.getItem('theme'));
